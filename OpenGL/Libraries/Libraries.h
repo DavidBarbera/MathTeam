0a57d15512c3b44401432e9a2512421c5c5877ec
David Barbera <davidbarbera9@gmail.com>
2016-01-11 01:41:25 +0000

    GLUI working
    OpenGL with GLUI window system based on freeglut.
    6 examples from documentation working ( with a few bugs)
    new Libraries specification and template created for Visual Studio 2015

--- OpenGL/Libraries/Libraries.h
+++ OpenGL/Libraries/Libraries.h
@@ -1,19 +1,50 @@
 #pragma once
 
 //libraries lib
-#pragma comment(lib, "../../Libraries/lib/freeglut.lib")
-#pragma comment(lib, "../../Libraries/lib/glew32.lib")
+#pragma comment(lib, "../../../Libraries/lib/glui32.lib")
+#pragma comment(lib, "../../../Libraries/lib/freeglut.lib")
+#pragma comment(lib, "../../../Libraries/lib/glew32.lib")
 
+
+
+//----------------------------------------------------------------------------------------------
 //OpenGL, latest version 4.5
-#include "../../Libraries/include/GL/glew.h"
+#include "../../../Libraries/include/GL/glew.h"
+#include "../../../Libraries/include/GL/wglew.h"
+
+/*
+//Initialize glew after creating an opengl context (i.e., creating a window with glut or others)
+//  (example: 'glutCreateWindow("my window");' would actually create the opengl context)
+// Then you can proceed to initialize glew this way (don't forget the first line):
+
+glewExperimental = GL_TRUE;
+GLenum err = glewInit();
+if (GLEW_OK != err)
+{
+fprintf(stderr, "Error initializing GLEW: %s\n",
+glewGetErrorString(err));
+}
+
+*/
+//------------------------------------------------------------------------------------------------
+
+
 //OpenGL, GLES 3.2
 //#include "../../Libraries/include/GLES/GLES3/gl32.h"
 //#include "../../Libraries/include/GLES/GLES3/gl2ext.h"
 //#include "../../Libraries/include/GLES/GLES3/gl3platform.h"
 
 //window manager freeglut
-#include "../../Libraries/include/GL/freeglut.h"
-#include "../../Libraries/include/GL/freeglut_ext.h"
+#include "../../../Libraries/include/GL/freeglut.h"
+#include "../../../Libraries/include/GL/freeglut_ext.h"
+#define GLUI_FREEGLUT
+#include "../../../Libraries/include/GL/glui.h"
+
+//GLM  math for OpenGL
+#define GLM_SWIZZLE
+#include "../../../Libraries/include/glm/glm.hpp"
+#include "../../../Libraries/include/glm/gtc/matrix_transform.hpp"
+#include "../../../Libraries/include/glm/gtx/transform2.hpp"
 
 //a math library i found
 //#include "../../Libraries/include/mathlib/vector.h"
@@ -35,9 +66,6 @@ using namespace std;
 //#include "../../Libraries/include/vec.h"
 //#include "../../Libraries/include/mat.h"
 
-
-//GLM  math for OpenGL
-#include "../../Libraries/include/glm/glm.hpp"
 
 //CML math library
 //#include "../../Libraries/include/cml/cml.h"
